9995b3f3f26538437d419553dbb8622de8677f4f
Ken Schlobohm <keschlob@microsoft.com>
2023-01-17 17:54:55 -0600

    fix build error for public web

--- src/Relecloud.Web.Public/Controllers/ConcertController.cs
+++ src/Relecloud.Web.Public/Controllers/ConcertController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Relecloud.Web.Models.ConcertContext;
+using Relecloud.Web.Models.Search;
+using Relecloud.Web.Models.Services;
 using Relecloud.Web.Public.Infrastructure;
 using Relecloud.Web.Public.ViewModels;
 
